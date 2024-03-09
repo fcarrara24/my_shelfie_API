c6e9128145df27598169c0582a34ff85bb225f29
Francesco Carrara <f.carrara24@studenti.unibg.it>
2024-03-09 19:46:59 +0100

    creating player for crud

---
+++ app/Http/Controllers/ApiControllers/PlayerController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\ApiControllers;
+
+use App\Models\Player;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PlayerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $players = Player::all();
+        return response()->json(['message' => 'all players', 'data' => $players], 201);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Player $player)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Player $player)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Player $player)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Player $player)
+    {
+        //
+    }
+}
